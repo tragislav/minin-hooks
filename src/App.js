6a331c5dce6f5cf7b13a7d8ecd6a4a76cffbb0cd
Tragislav Work PC <s.dykomenko@gmail.com>
2021-12-27 16:54:41 +0300

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
+import React, {
+    useState,
+    useEffect,
+    useRef,
+    useMemo,
+    useCallback,
+} from 'react';
+import ItemsList from './ItemsList';
 
 function App() {
     const [colored, setColored] = useState(false);
@@ -8,9 +15,13 @@ function App() {
         color: colored ? 'darkred' : 'black',
     };
 
+    const generateItemsFromAPI = useCallback(() => {
+        return new Array(count).fill('').map((_, i) => `Элемент ${i + 1}`);
+    }, [count]);
+
     return (
         <>
-            <h1 style={styles}>Количество элементов: </h1>
+            <h1 style={styles}>Количество элементов: {count}</h1>
             <button
                 className="btn btn-success"
                 onClick={() => setCount((prev) => prev + 1)}
@@ -23,6 +34,8 @@ function App() {
             >
                 Изменить
             </button>
+
+            <ItemsList getItems={generateItemsFromAPI} />
         </>
     );
 }
