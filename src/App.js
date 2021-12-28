e3b33009ccceb329a9248b324218d9a1baec926f
Tragislav Work PC <s.dykomenko@gmail.com>
2021-12-28 11:17:24 +0300

    custom hooks useInput and useLogger

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import React, {
-    useState,
-    useEffect,
-    useRef,
-    useMemo,
-    useCallback,
-} from 'react';
-import Alert from './alert/Alert';
-import { AlertProvider } from './alert/AlertContext';
-import Main from './Main';
+import React, { useEffect, useState } from 'react';
+
+function useLogger(value) {
+    useEffect(() => {
+        console.log('Value changed: ', value);
+    }, [value]);
+}
+
+function useInput(initialValue) {
+    const [value, setValue] = useState(initialValue);
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    const clear = () => setValue('');
+
+    return {
+        bind: {
+            value,
+            onChange,
+        },
+        value,
+        clear,
+    };
+}
 
 function App() {
+    const input = useInput('');
+
+    useLogger(input.value);
+
     return (
-        <AlertProvider>
-            <div className={'container pt-3'}>
-                <Alert />
-                <Main toggle={() => {}} />
-            </div>
-        </AlertProvider>
+        <div className={'container pt-3'}>
+            <input type="text" {...input.bind} />
+            <button className="btn btn-warning" onClick={() => input.clear()}>
+                Очистить
+            </button>
+            <hr />
+            <h1>{input.value}</h1>
+        </div>
     );
 }
 
